7d5e7ad60b9f361882c70a8f163ec2a956e968ac
Deleanu Radu <radu_deleanu@outlook.com>
2023-02-19 01:30:23 +0200

    celanu-up

--- src/app/modules/gallery/gallery-page/gallery-page.component.ts
+++ src/app/modules/gallery/gallery-page/gallery-page.component.ts
@@ -23,6 +23,22 @@ export class GalleryPageComponent {
     {
       imageUrl: 'https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU',
       artistName: 'Go Wild'
+    },
+    {
+      imageUrl: 'https://fastly.picsum.photos/id/15/2500/1667.jpg?hmac=Lv03D1Y3AsZ9L2tMMC1KQZekBVaQSDc1waqJ54IHvo4',
+      artistName: 'Paul Jarvis'
+    },
+    {
+      imageUrl: 'https://fastly.picsum.photos/id/25/5000/3333.jpg?hmac=yCz9LeSs-i72Ru0YvvpsoECnCTxZjzGde805gWrAHkM',
+      artistName: 'Alejandro Escamilla'
+    },
+    {
+      imageUrl: 'https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA',
+      artistName: 'Jerry Adney'
+    },
+    {
+      imageUrl: 'https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU',
+      artistName: 'Go Wild'
     }
   ]
 }
